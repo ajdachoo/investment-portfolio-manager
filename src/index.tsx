ad4a2a49238132c7b9d9c2d9bd5165bb24aea689
Adam Cieslak <adamcieslak00@gmail.com>
2024-02-16 16:47:00 +0100

    add basic layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './views/App';
+import { MantineProvider } from '@mantine/core';
+import { theme } from "./assets/styles/theme";
+import '@mantine/core/styles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme} defaultColorScheme='light'>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { AppShell, Avatar, Button, Flex, useMantineTheme } from '@mantine/core';
+
 
 function App() {
+  const theme = useMantineTheme();
+
+
   return (
-    <div>
-    </div>
+    <AppShell
+      layout='default'
+      header={{ height: 90 }}
+      navbar={{
+        width: 200,
+        breakpoint: 0,
+      }}
+      padding='md'
+    >
+      <AppShell.Header>
+        <Flex
+          justify='space-between'
+          align='center'
+          gap='md'
+          p='md'
+        >
+          <p>InvestmentPortfolioManager</p>
+          <Flex
+            align='center'
+            gap='sm'
+          >
+            <p>Name Surname</p>
+            <Avatar color={theme.primaryColor}>AC</Avatar>
+          </Flex>
+        </Flex>
+      </AppShell.Header>
+      <AppShell.Navbar
+        p='md'
+      >
+        <Button variant='filled' color="yellow">Dupa</Button>
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <Button variant='filled'>Dupa</Button>
+      </AppShell.Main>
+    </AppShell>
   );
 }
 
